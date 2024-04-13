1a55abb200c73f8badae8cbe99409c6ca1ae7e69
roblusk <130242918+roblusk@users.noreply.github.com>
2024-04-13 00:31:13 +0000

    finished director info page

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -5,6 +5,7 @@ class DirectorsController < ApplicationController
   def show
     the_id = params.fetch("the_id")
     @the_director = Director.where({id: the_id})[0]
+    @movies = Movie.where({ :director_id => @the_director.id})
     render({ :template => "director_templates/details"})
   end
 end
